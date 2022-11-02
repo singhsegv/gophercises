06d46b085667a5da233320c9100586c454895021
rajdeep1008 <iamrajdeep1008@gmail.com>
2022-11-02 23:43:24 +0530

    Basic AF solution for quiz game part 2

--- quiz/main.go
+++ quiz/main.go
@@ -9,11 +9,16 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"sync"
+	"time"
 )
+
+var wg sync.WaitGroup
 
 type quizConfig struct {
 	filename string
 	shuffle  bool
+	time     int
 }
 
 type problem struct {
@@ -25,6 +30,7 @@ type quiz struct {
 	totalProblems    int
 	correctSolutions int
 	shouldShuffle    bool
+	timeout          int
 }
 
 var config quizConfig
@@ -32,10 +38,11 @@ var config quizConfig
 func init() {
 	flag.StringVar(&config.filename, "csv", "problems.csv", "path for a csv file in the format 'question,answer'")
 	flag.BoolVar(&config.shuffle, "shuffle", false, "true/false flag to shuffle order of questions")
+	flag.IntVar(&config.time, "time", 30, "time in seconds after which the quiz ends")
 	flag.Parse()
 }
 
-func NewQuiz(input io.Reader, shouldShuffle bool) (*quiz, error) {
+func NewQuiz(input io.Reader, shouldShuffle bool, timeout int) (*quiz, error) {
 	reader := csv.NewReader(input)
 
 	records, err := reader.ReadAll()
@@ -57,6 +64,7 @@ func NewQuiz(input io.Reader, shouldShuffle bool) (*quiz, error) {
 		totalProblems:    len(problems),
 		correctSolutions: 0,
 		shouldShuffle:    shouldShuffle,
+		timeout:          timeout,
 	}
 
 	return &q, nil
@@ -76,10 +84,20 @@ func (q *quiz) shuffle() {
 	q.problems = reorderedProblems
 }
 
+func (q *quiz) startTimer() {
+	defer wg.Done()
+
+	time.Sleep(time.Duration(q.timeout) * time.Second)
+}
+
 func (q *quiz) Run() {
+	defer wg.Done()
+
 	if q.shouldShuffle {
 		q.shuffle()
 	}
+
+	go q.startTimer()
 
 	for i := 0; i < q.totalProblems; i++ {
 		var answer string
@@ -91,6 +109,7 @@ func (q *quiz) Run() {
 			q.correctSolutions += 1
 		}
 	}
+	wg.Done()
 }
 
 func (q *quiz) PrintResults() {
@@ -104,11 +123,17 @@ func main() {
 	}
 	defer quizFile.Close()
 
-	quiz, err := NewQuiz(quizFile, config.shuffle)
+	quiz, err := NewQuiz(quizFile, config.shuffle, config.time)
 	if err != nil {
 		log.Fatalf("Failed to parse file.\nError: %s\n", err)
 	}
 
-	quiz.Run()
+	fmt.Println("Press enter to start the quiz")
+	fmt.Scanf("%s")
+
+	wg.Add(1)
+	go quiz.Run()
+
+	wg.Wait()
 	quiz.PrintResults()
 }
